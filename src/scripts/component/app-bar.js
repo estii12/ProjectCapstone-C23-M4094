3b52f1788ee9fa72a760119a7a223cf0c18ce20a
estii12 <esti.20007@mhs.unesa.ac.id>
2023-06-13 21:48:47 +0700

    Update App-Bar

---
+++ src/scripts/component/app-bar.js
@@ -0,0 +1,30 @@
+class AppBar extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+        <header>
+            <div class="all-nav">
+            <div class="logo">
+                <a href="/" class="navbar-brand"><img src="./images/keepme/logo1.png" alt="KeepMe">KeepMe</a>
+            </div>
+            <button id="hamburger" class="hamburger" title="Accept requested meeting.">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </button>
+            <nav id="nav-bar" class="nav-bar">
+                <ul>
+                <li><a class="active" href="/">Beranda</a></li>
+                <li><a href="/#/article">Artikel</a></li>
+                <li><a href="/#/tentang-kami">Tentang Kami</a></li>
+                <li><a class="btn-login" href="/#/login-user">Masuk</a></li>
+                </ul>
+            </nav>
+            </div>
+        </header>
+            `;
+    }
+}
+
+customElements.define('app-bar', AppBar);
